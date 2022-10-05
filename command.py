e81f67f99d2b2c8764de2c6a7af0b0d0cc5bb2d5
IvanSavkin <adoys1@gmail.com>
2022-10-05 22:03:50 +0300

    end HW

--- command.py
+++ command.py
@@ -1,8 +1,6 @@
 
 from ui import hello_user
-from studens import students_list
-from stunent_class import class_list
-
+from random import  randint
 def search():
     while True:
         hello_user()
@@ -20,28 +18,30 @@ def search():
             input('\nНажмите enter')
 
         elif n == 3:
-            answer = str(input('Введите фамилию ученика: '))
-            if answer in students_list['Фамилия']:
-                index = students_list['Фамилия'].index(answer)
-            print(f"{students_list['ID'][index]}, {students_list['Имя'][index]} {students_list['Фамилия'][index]}, {students_list['Дата рождения'][index]}, {students_list['Успеваемость'][index]}")
-            input('\nНажмите enter')
-        
+            answer = input('Введите фамилию ученика: ').title()
+            with open('students.csv', 'r', encoding='utf_8') as f:
+                for line in f.readlines():
+                    if answer in line:
+                        a = line.split()
+                        print(a[3])
+
         elif n == 4:
             f = open('students.csv','a+', encoding = 'utf_8')
             new_contact = input('Введите данные(ФИ, год рождения, успеваемость, ID) ').split(' ')
-            f.write('   |'.join(new_contact))
+            f.write('\n'+'\t'.join(new_contact))
             f.close()
-            print(f.readlines())
-            # students_list['ID'] = new_contact[0]
             input('\nНажмите enter')
 
         elif n == 5:
-            answer_name = input('Ввдите фамилию ученика: ')
-            if answer_name in students_list['Фамилия']:
-                index = students_list['Фамилия'].index(answer_name)
-            print(f"Учник {students_list['Фамилия'][index]} {students_list['Имя'][index]} находится в кабинете {class_list['Кабинет'][index]} на уроке {class_list['Урок'][index]}, этаж {class_list['Этаж'][index]}")
-            input('\nНажмите enter')
+            answer = input("Введите Фамилию: ")
+            with open('class.csv', 'r', encoding='utf_8') as f:
+                a = len(f.readlines())
 
+            f = open('students.csv', 'r', encoding='utf_8')
+            for line in f.readlines():
+                if answer in line:
+                    with open('class.csv', 'r', encoding='utf_8') as f:
+                        print(f.readlines()[randint(1,a-1)])
 
         elif n == 6:
             print('До встречи')
